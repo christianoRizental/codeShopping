4f12c93f9085bab88cd5469546df1c51ce49c16f
Christiano Rizental <rizental@gmail.com>
2018-07-03 15:04:17 -0300

    arrumado whit para with

--- app/Http/Controllers/Api/ProductInputController.php
+++ app/Http/Controllers/Api/ProductInputController.php
@@ -12,17 +12,24 @@ class ProductInputController extends Controller
 {
     public function index()
     {
-        //whit -> acessa menos o banco de dados
-        $inputs = ProductInput::whit('product')->paginate(); //eager loading/ carregamento prematuro
+        //with -> acessa menos o banco de dados
+        $inputs = ProductInput::with('product')->paginate(); //eager loading/ carregamento prematuro
         return ProductInputResource::collection($inputs);
     }
 
     public function store(ProductInputRequest $request)
     {
         $input = ProductInput::create($request->all());
-        $product = $input->product;
-        $product->stock += $input->amount;
-        $product->save();
+        //$product = $input->product;
+        //$product->stock += $input->amount;
+        //$product->save();
+        //codigo acima foi colocado no evento da criação da entrada no AppServiceProvider
+        //evento é algo que ocorreu - vigia - executar algo -> tarefas
+        //não vai só atualizar o estoque
+        //pode enviar e-mail pra gestor/gerente
+        //alguma outra tarefa
+        //outra tarefa ainda
+        //mais outra tarefa
         return new ProductInputResource($input);
     }
 
