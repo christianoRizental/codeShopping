cd8282531ea217443674343de81f0f0344667858
Christiano Rizental <rizental@gmail.com>
2018-09-10 17:53:17 -0300

    Iniciando com Angular

---
+++ _angular-app/src/app/components/pages/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public credentials = {
+      email: '',
+      password: ''
+  }
+  
+  //simbolo [] - o TS reflete alterações no template - Dados ---> template
+  //           - a programação irá alterar o template
+  
+  constructor(private http: HttpClient) { //injeção de dependencia automatica
+  }
+
+  ngOnInit() {
+    /*
+    setTimeout( () => {
+        this.email = 'qualquer coisa';
+    }, 3000)
+    */
+  }
+  
+  submit(){
+      this.http.post('http://localhost:8000/api/login', this.credentials)
+      .subscribe((data) => console.log(data));
+      //enviar uma requisição ajax com as credenciais 
+      return false;
+  }
+}
